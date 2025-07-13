ec030e55fccb34cb2d67639ff964520102a18a2f
sanjaykumar <sanjaypro2003@gmail.com>
2025-07-13 19:42:34 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css'
 import Navbar from './components/header/Navbar'
 import Home from './components/pages/Home'
